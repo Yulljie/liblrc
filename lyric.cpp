a7e20492eee9f767f1b915a2d9aa94eab1efb31b
Yulliil <169875254@qq.com>
2026-05-04 18:15:52 +0800

    bahavior and comment enhancement

--- lyric.cpp
+++ lyric.cpp
@@ -13,26 +13,22 @@ int32_t Lyric::get_time(int32_t index) const {
 }
 
 int32_t Lyric::get_index(int32_t ms) const {
-	if (lines.empty()) return -1;
-
+	if (!m_is_sorted || lines.empty()) return -1;
+	
 	auto next = std::upper_bound(lines.begin(), lines.end(), ms, [](int32_t t, const line& l) {return t < l.time;});
 	return std::distance(lines.begin(), next) - 1;
 }
 
 const std::string& Lyric::get_line_by_index(int32_t index) const {
-	assert(index < lines.size() && "Index out of bound!");
-	return lines[index].content;
+	return (index >= 0 && index < size()) ? lines[index].content : EMPTY_STRING;
 }
 
 const std::string& Lyric::get_line_by_time(int32_t ms) const {
-	int32_t index = get_index(ms);
-	return (index != -1) ? get_line_by_index(index) : EMPTY_STRING;
+	return get_line_by_index(get_index(ms));
 }
 
 int32_t Lyric::offset(int32_t ms) {
-	if (lines.empty()) return 1;
 	for (uint32_t i = 0; i < lines.size(); i++) lines[i].time += ms;
-	return 0;
 }
 
 int32_t Lyric::add(int32_t time, std::string content);
--- lyric.h
+++ lyric.h
@@ -19,7 +19,7 @@ class Lyric {
 		// For err use
 		static const std::string EMPTY_STRING;
 		
-		bool m_is_sorted = false;
+		bool m_is_sorted = true;
 	public:
 		// Create/Initialize a new Lyric container
 		Lyric();
@@ -27,31 +27,31 @@ class Lyric {
 		// Return amount of saved lines
 		uint32_t size() const;
 
-		// Basic feature, DO NOT pass index larger than count() - 1 !!!
+		// Basic feature, this will NOT check m_is_sorted, DO NOT pass index larger than count() - 1 !!!
 		int32_t get_time(int32_t index) const;
 
-		// Basic feature, return -1 if failed
+		// Basic feature, return -1 if failed, this will check m_is_sorted.
 		int32_t get_index(int32_t ms) const;
 
-		// Basic feature. Return blank string if nothing found. DO NOT pass index larger than count() - 1 !!!
+		// Basic feature, this will NOT check m_is_sorted(), this will return EMPTY_STRING if invalid index passed (Because the line itself could be empty, check the boundary yourself!!!)
 		const std::string& get_line_by_index(int32_t index) const;
 
-		// Basic feature. Return blank string if nothing found.
+		// Basic feature, this is implemented as get_line_by_index(get_index(ms)).
 		const std::string& get_line_by_time(int32_t ms) const;
 
-		// return 0 if nothing bad, 1 if nothing changed (to a blank container), and nothing to err?
-		int32_t offset(int32_t ms);
+		// Set offset to all lines. this will not err, ig. This will NOT modify m_is_sorted.
+		void offset(int32_t ms);
 		
-		// Add line to a Lyric, liblrc will not automatically sort 
+		// Add line to a Lyric, liblrc will not automatically sort, this will set m_is_sorted to false
 		int32_t add(int32_t time, std::string content);
 
-		// Delete line from a Lyric by index
+		// Delete line from a Lyric by index, this will not modify m_is_sorted.
 		int32_t del(int32_t index);
 		
-		// Sort lyrics by time, return 0 if nothing bad, -1 if failed.
+		// Sort lyrics by time, return 0 if nothing bad, -1 if failed. This will set m_is_sorted to true if nothing bad.
 		int32_t sort();
 
-		// Set is_sorted to true, without doing anything.
+		// !!!DANGEROUS!!! Set m_is_sorted to true, without doing anything.
 		void force_sorted();
         
 };
