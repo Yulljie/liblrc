b961fd637ff3c293e634453ba8b129f579964d62
Yulliil <169875254@qq.com>
2026-05-04 16:39:43 +0800

    Add m_is_sorted, add related sort() and force_sorted(), modify bahavior of add() in comment

--- lyric.h
+++ lyric.h
@@ -18,9 +18,10 @@ class Lyric {
 		
 		// For err use
 		static const std::string EMPTY_STRING;
-
+		
+		bool m_is_sorted = false;
 	public:
-		// Create new Lyric 
+		// Create/Initialize a new Lyric container
 		Lyric();
         
 		// Return amount of saved lines
@@ -41,11 +42,17 @@ class Lyric {
 		// return 0 if nothing bad, 1 if nothing changed (to a blank container), and nothing to err?
 		int32_t offset(int32_t ms);
 		
-		// Add line to a Lyric, liblrc will automatically sort 
+		// Add line to a Lyric, liblrc will not automatically sort 
 		int32_t add(int32_t time, std::string content);
 
 		// Delete line from a Lyric by index
 		int32_t del(int32_t index);
+		
+		// Sort lyrics by time, return 0 if nothing bad, -1 if failed.
+		int32_t sort();
+
+		// Set is_sorted to true, without doing anything.
+		void force_sorted();
         
 };
 
